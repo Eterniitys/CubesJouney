0189ad8f3cf4d4fbdfe067b223f27635a57785b1
Eterniitys <johan.guerrero394@gmail.com>
2019-09-12 12:49:01 +0000

    after tuto n°4

---
+++ code/grousk.gd
@@ -0,0 +1,47 @@
+extends KinematicBody2D
+
+const GRAVITY = 1000
+const UP = Vector2(0,-1)
+const ACCEL = 5
+
+var vel = Vector2()
+export(int) var max_speed = 200
+var dirx = 0
+
+func _ready():
+	randomize()
+
+func _on_time_timeout():
+	var m = int(rand_range(0,8))
+	if m < 2:
+		dirx = -1
+	elif m > 5:
+		dirx = 1
+	else:
+		dirx = 0  
+	
+func _physics_process(delta):
+	vel.y += GRAVITY * delta
+	movement_loop()
+
+# warning-ignore:return_value_discarded
+	move_and_slide(vel, UP) # UP indique la direction du plafond
+
+func movement_loop():
+
+	match dirx: # switch
+		1: # Droite
+			vel.x = min(vel.x + ACCEL, max_speed)
+			$Sprite.flip_h = false
+			$anim.play("walk")
+		-1: # Gauche
+			vel.x = max(vel.x - ACCEL, -max_speed)
+			$Sprite.flip_h = true
+			$anim.play("walk")
+		_: # Both / Nothing
+			vel.x = lerp(vel.x ,0, 0.2)
+			$anim.play("idle")
+
+	if is_on_wall():
+		$anim.play("idle")
+
--- code/player.gd
+++ code/player.gd
@@ -2,10 +2,10 @@ extends KinematicBody2D
 
 const GRAVITY = 1000
 const UP = Vector2(0,-1)
-const ACCEL = 5
+const ACCEL = 10
 
 var vel = Vector2()
-var max_speed = 200
+export(int) var max_speed = 300
 
 func _ready():
 	pass
@@ -16,26 +16,34 @@ func _process(delta):
 func _physics_process(delta):
 	vel.y += GRAVITY * delta
 	movement_loop()
-	
+
 	move_and_slide(vel, UP) # UP indique la direction du plafond
-	
+
 func movement_loop():
 	var right = Input.is_action_pressed("ui_right")
 	var left = Input.is_action_pressed("ui_left")
 	var dirx = int(right) - int(left)
 	match dirx: # switch
 		1: # Droite
-			vel.x = min(vel.x+ACCEL, max_speed)
+			vel.x = min(vel.x + ACCEL, max_speed)
+			$Sprite.flip_h = false
+			$anim.play("walk")
 		-1: # Gauche
-			vel.x = max(vel.x-ACCEL, -max_speed)
+			vel.x = max(vel.x - ACCEL, -max_speed)
+			$Sprite.flip_h = true
+			$anim.play("walk")
 		_: # Both
 			vel.x = lerp(vel.x ,0, 0.2)
-	
-	 #vel.x = max_speed * dirx
-	
+			$anim.play("idle")
+
 	var jump = Input.is_action_just_pressed("ui_up")
 	if jump == true and is_on_floor():
-		vel.y = -300
-		
+		vel.y = -600
+
 	if !jump and is_on_floor():
 		vel.y = 100
+	
+	if vel.y < 0:
+		$anim.play("jump_up")
+	elif vel.y > 0 and vel.y != 100:
+		$anim.play("jump_down")
