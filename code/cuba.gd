890dd061cd001eb358f5929add67ca00995d3126
Eterniitys <johan.guerrero394@gmail.com>
2019-09-29 20:38:20 +0000

    cubi&cuba able to jump together + carried each other

--- code/cuba.gd
+++ code/cuba.gd
@@ -4,15 +4,16 @@ const UP = Vector2.UP
 const GRAVITY = 1000
 const MAX_SPEED = 300
 const JUMP_HEIGH = 550
-# Velocity
+# movements
+enum {IDLE, TRANSLATE, JUMP}
 var vel = Vector2()
+var carried = false
+signal wanna_jump
 # Shadow
-#var shadow_preload = preload("res://scene/objects/cubx_shadow.tscn")
 var shadow
 # TODO del
 var old_scale = false
-enum {IDLE, TRANSLATE, JUMP}
-var state = IDLE
+var state
 var snap
 # Scaling cube datas
 var scale_speed = 0.15
@@ -22,8 +23,9 @@ var delta_scale_x
 var delta_scale_y
 
 func _ready():
-	#shadow = shadow_preload.instance()
 	shadow = get_parent().get_node("cubx_shadow")
+	get_parent().get_node("cubi").connect("wanna_jump", self, "_cubi_wanna_jump")
+	change_state(IDLE)
 
 func _physics_process(delta):
 	# Gravity
@@ -40,40 +42,46 @@ func _physics_process(delta):
 	# Shadow power
 	if Input.is_action_just_pressed("shadow_cuba"):
 		call_shadow()
+	# Going through tiles (allowing it)
+	travers()
 	
 	# Moves applications
 	vel = move_and_slide_with_snap(vel, snap, UP)
-	# Travers cube when press f/j
-	travers()
 
 
 func movements(delta):
+	for i in get_slide_count():
+		var col = get_slide_collision(i)
+		var has_cubi = false
+		if col.collider.name == "cubi" and col.normal == Vector2.UP:
+			has_cubi = true
+			carried = true
+		if !has_cubi:
+			carried = false
 	
 	var left = Input.is_action_pressed("left_cuba")
 	var right = Input.is_action_pressed("right_cuba")
-	var dirx = int(right) - int(left)
-	
-	if dirx == 1:
-		vel.x = MAX_SPEED 
-	elif dirx == -1:
-		vel.x = -MAX_SPEED
-	else:
-		vel.x = 0
+	# direction
+	if left and !right: vel.x = -MAX_SPEED 
+	elif right and !left: vel.x = MAX_SPEED
+	else: vel.x = 0
 	
 	var jump = Input.is_action_just_pressed("jump_cuba")
 	if jump and is_on_floor():
+		emit_signal("wanna_jump")
+		yield(get_tree().create_timer(0.02),"timeout")
 		vel.y = -JUMP_HEIGH
-		
+
 func travers ():
 	if Input.is_action_just_pressed("travers_cuba"):
 		set_collision_mask_bit(5,false)
 	if Input.is_action_just_released("travers_cuba"):
-		set_collision_mask_bit(5,true)	
-		
+		set_collision_mask_bit(5,true)
+
 func call_shadow():
-	shadow.set_collision_layer_bit(11,false)
+	shadow.set_collision_layer_bit(10,false)
 	shadow.get_node("sprite").region_rect = $Sprite.region_rect
-	shadow.set_collision_layer_bit(10,true)
+	shadow.set_collision_layer_bit(11,true)
 	shadow.scale = scale
 	shadow.position = position
 	#get_parent().add_child(shadow)
@@ -90,14 +98,14 @@ func manage_state():
 
 func change_state(new_state):
 	state = new_state
-	#print("idle" if state == IDLE else ("translate" if state == TRANSLATE else "jump"))	
+	#print("idle" if state == IDLE else ("translate" if state == TRANSLATE else "jump"))
 	match state:
 		IDLE:
 			snap = Vector2(0,32)
 		TRANSLATE:
 			pass
 		JUMP:
-			snap = Vector2(0,0)
+			snap = Vector2.ZERO
 
 func transform_alt(delta):
 	if Input.is_action_just_pressed("transform_down_cuba"):
@@ -109,7 +117,7 @@ func transform_alt(delta):
 				state = "down"
 			scale.x += delta_scale_x
 			scale.y -= delta_scale_y
-
+	
 	if Input.is_action_just_pressed("transform_up_cuba"):
 		
 		if state != "up":
@@ -121,14 +129,17 @@ func transform_alt(delta):
 			scale.y += delta_scale_y
 
 func transform(delta):
-	
 	if Input.is_action_just_pressed("transform_down_cuba") and is_on_floor() and (scale.y < 0.5) :
 		vel.y = -200
-		
+	
 	if Input.is_action_pressed("transform_down_cuba"):
 		scale.x = lerp (scale.x, scale_min_x, scale_speed)
 		scale.y = lerp (scale.y, scale_min_y, scale_speed)
-
+	
 	if Input.is_action_pressed("transform_up_cuba"):
 		scale.x = lerp (scale.x, scale_min_x + delta_scale_x, scale_speed)
 		scale.y = lerp (scale.y, scale_min_y + delta_scale_y, scale_speed)
+
+func _cubi_wanna_jump():
+	if carried :
+		vel.y = -JUMP_HEIGH
--- code/cubi.gd
+++ code/cubi.gd
@@ -4,15 +4,16 @@ const UP = Vector2.UP
 const GRAVITY = 1000
 const MAX_SPEED = 300
 const JUMP_HEIGH = 550
-# Velocity
+# movementd
+enum {IDLE, TRANSLATE, JUMP}
 var vel = Vector2()
+var carried = false
+signal wanna_jump
 # Shadow
-#var shadow_preload = preload("res://scene/objects/cubx_shadow.tscn")
 var shadow
 # TODO del
 var old_scale = false
-enum {IDLE, TRANSLATE, JUMP}
-var state = IDLE
+var state
 var snap
 # Scaling cube datas
 var scale_speed = 0.15
@@ -22,8 +23,9 @@ var delta_scale_x
 var delta_scale_y
 
 func _ready():
-	#shadow = shadow_preload.instance()
 	shadow = get_parent().get_node("cubx_shadow")
+	get_parent().get_node("cuba").connect("wanna_jump", self, "_cuba_wanna_jump")
+	change_state(IDLE)
 
 func _physics_process(delta):
 	# Gravity
@@ -40,43 +42,47 @@ func _physics_process(delta):
 	# Shadow power
 	if Input.is_action_just_pressed("shadow_cubi"):
 		call_shadow()
+	# Going through tiles (allowing it)
+	travers()
 	
 	# Moves applications
 	vel = move_and_slide_with_snap(vel, snap, UP)
-	# Travers cube when press f/j
-	travers()
-
 
 func movements(delta):
+	for i in get_slide_count():
+		var col = get_slide_collision(i)
+		var has_cubi = false
+		if col.collider.name == "cuba" and col.normal == Vector2.UP:
+			has_cubi = true
+			carried = true
+		if !has_cubi:
+			carried = false
 	
 	var left = Input.is_action_pressed("left_cubi")
 	var right = Input.is_action_pressed("right_cubi")
-	var dirx = int(right) - int(left)
-	
-	if dirx == 1:
-		vel.x = MAX_SPEED 
-	elif dirx == -1:
-		vel.x = -MAX_SPEED
-	else:
-		vel.x = 0
+	# direction
+	if left and !right: vel.x = -MAX_SPEED 
+	elif right and !left: vel.x = MAX_SPEED
+	else: vel.x = 0
 	
 	var jump = Input.is_action_just_pressed("jump_cubi")
 	if jump and is_on_floor():
+		emit_signal("wanna_jump")
+		yield(get_tree().create_timer(0.02),"timeout")
 		vel.y = -JUMP_HEIGH
-		
+
 func travers ():
 	if Input.is_action_just_pressed("travers_cubi"):
 		set_collision_mask_bit(5,false)
 	if Input.is_action_just_released("travers_cubi"):
 		set_collision_mask_bit(5,true)	
-		
+
 func call_shadow():
 	shadow.set_collision_layer_bit(11,false)
 	shadow.get_node("sprite").region_rect = $Sprite.region_rect
 	shadow.set_collision_layer_bit(10,true)
 	shadow.scale = scale
 	shadow.position = position
-	#get_parent().add_child(shadow)
 
 func manage_state():
 	if state == IDLE and vel.x != 0:
@@ -97,7 +103,8 @@ func change_state(new_state):
 		TRANSLATE:
 			pass
 		JUMP:
-			snap = Vector2(0,0)
+			snap = Vector2.ZERO
+			carried = false
 
 func transform_alt(delta):
 	if Input.is_action_just_pressed("transform_down_cubi"):
@@ -109,7 +116,7 @@ func transform_alt(delta):
 				state = "down"
 			scale.x += delta_scale_x
 			scale.y -= delta_scale_y
-
+	
 	if Input.is_action_just_pressed("transform_up_cubi"):
 		
 		if state != "up":
@@ -121,14 +128,17 @@ func transform_alt(delta):
 			scale.y += delta_scale_y
 
 func transform(delta):
-	
 	if Input.is_action_just_pressed("transform_down_cubi") and is_on_floor() and (scale.y < 0.5) :
 		vel.y = -200
-		
+	
 	if Input.is_action_pressed("transform_down_cubi"):
 		scale.x = lerp (scale.x, scale_min_x, scale_speed)
 		scale.y = lerp (scale.y, scale_min_y, scale_speed)
-
+	
 	if Input.is_action_pressed("transform_up_cubi"):
 		scale.x = lerp (scale.x, scale_min_x + delta_scale_x, scale_speed)
 		scale.y = lerp (scale.y, scale_min_y + delta_scale_y, scale_speed)
+
+func _cuba_wanna_jump():
+	if carried :
+		vel.y = -JUMP_HEIGH
