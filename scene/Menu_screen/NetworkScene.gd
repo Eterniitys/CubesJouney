2ed8144192f7b54abaecfc597536ebe318dce6a8
Eterniitys <johan.guerrero394@gmail.com>
2019-11-24 16:39:32 +0100

    fix network scene return button

--- scene/Menu_screen/NetworkScene.gd
+++ scene/Menu_screen/NetworkScene.gd
@@ -1,5 +1,9 @@
 extends Control
 
+func _ready():
+	var btn_return = $Menu/Content/Buttons/ReturnMenuButton
+	btn_return.connect("pressed", self, "_on_ButtonReturn_pressed", [btn_return.next_scene])
+
 func _load_game():
 	get_tree().change_scene("res://scene/levels/waitLevel.tscn")
 
@@ -10,3 +14,9 @@ func _on_ConnectToServer_pressed():
 func _on_CreateServer_pressed():
 	_load_game()
 	NETWORK.create_server("cubi")
+
+func _on_ButtonReturn_pressed(scene_to_load):
+	$FadeIn.show()
+	$FadeIn/AnimationPlayer.play("Fade");
+	yield($FadeIn/AnimationPlayer, "animation_finished")
+	get_tree().change_scene(scene_to_load)
