75bbef9e0cbcb9b437483752c2daed43270a5a1e
Eterniitys <johan.guerrero394@gmail.com>
2019-11-29 16:44:17 +0100

    add quit button + gamepad control on menu

--- scene/Menu_screen/TitleScreen.gd
+++ scene/Menu_screen/TitleScreen.gd
@@ -1,12 +1,16 @@
 extends Control
 #warning-ignore:return_value_discarded
 func _ready():
-	#$Menu/Content/Buttons/Play_button.grab_focus()
+	$Menu/Content/Buttons.get_children()[1].grab_focus()
 	for button in $Menu/Content/Buttons.get_children():
-		button.connect("pressed", self, "_on_button_pressed", [button.next_scene])
+		if button.next_scene != null :
+			button.connect("pressed", self, "_on_button_pressed", [button.next_scene])
 
 func _on_button_pressed(scene_to_load):
 	$FadeIn.show()
 	$FadeIn/AnimationPlayer.play("Fade");
 	yield($FadeIn/AnimationPlayer, "animation_finished")
 	get_tree().change_scene_to(scene_to_load)
+
+func _on_QuitButton_pressed():
+	get_tree().quit()
